c5f1edfeaa77b351a918ab5c8096d93ea7dcc682
Sandeep kumar saw <sandeepkr1084@gmail.com>
2022-04-10 20:56:04 +0530

    updates to deploy to heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ZoomComponent } from './zoom/zoom.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'appointment',
+    component: ZoomComponent,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ZoomComponent } from './zoom/zoom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZoomComponent
   ],
   imports: [
     BrowserModule,
