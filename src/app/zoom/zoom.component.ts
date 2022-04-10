beb4d3d152b0296ce905d0aeb9ab1a6aa5485e04
Sandeep kumar saw <sandeepkr1084@gmail.com>
2022-04-10 23:58:42 +0530

    zoom test

--- src/app/zoom/zoom.component.ts
+++ src/app/zoom/zoom.component.ts
@@ -10,7 +10,7 @@ export class ZoomComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.open(`https://zoom.us/oauth/authorize?response_type=code&client_id=WBd6tpAsR2iZwgGdfzGsuQ&redirect_uri=https%3A%2F%2Fwebdev-test-1.herokuapp.com%2Fappointment`, "_self")
+    window.open(`https://zoom.us/oauth/authorize?response_type=code&client_id=WBd6tpAsR2iZwgGdfzGsuQ&redirect_uri=https://webdev-test-1.herokuapp.com/appointment`, "_self")
   
   }
 
