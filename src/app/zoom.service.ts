1462c5a0843a89216f3f5dc0ee804562f29e5fff
Sandeep kumar saw <sandeepkr1084@gmail.com>
2022-04-11 21:22:38 +0530

    zoom test token

--- src/app/zoom.service.ts
+++ src/app/zoom.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Base64 } from 'js-base64' ;
+import { ZoomMtg } from '@zoomus/websdk';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,15 @@ export class ZoomService {
   constructor(
     private http: HttpClient, 
   ) { }
+
+  signatureEndpoint = 'http://localhost:4200'
+  apiKey = 'JWT_API_KEY'
+  meetingNumber = 123456789
+  role = 0
+  leaveUrl = 'http://localhost:4200'
+  userName = 'WebSDK'
+  userEmail = ''
+  passWord = ''
 
   getZoomHeader() {
     const token = localStorage.getItem('token');
@@ -32,4 +42,53 @@ export class ZoomService {
     //let cal_data = `client_id=${environment.OutlookCalendarCred.client_id}&scope=${environment.OutlookCalendarCred.scope}&code=${code}&redirect_uri=${environment.OutlookCalendarCred.redirect_uri}&grant_type=${environment.OutlookCalendarCred.grant_type}&client_secret=${environment.OutlookCalendarCred.client_secret}`
     return this.http.post<any>(`https://zoom.us/oauth/token?code=${code}&grant_type=authorization_code&redirect_uri=https://webdev-test-1.herokuapp.com/appointment`, cal_data, {headers: this.getZoomHeader()})
   }
+
+  getSignature() {
+
+    this.http.post(this.signatureEndpoint, {
+       meetingNumber: this.meetingNumber,
+       role: this.role
+    }).toPromise().then((data: any) => {
+       if(data.signature) {
+          console.log(data.signature)
+          this.startMeeting(data.signature)
+       } else {
+          console.log(data)
+       }
+    }).catch((error) => {
+       console.log(error)
+    })
+ 
+ }
+
+ startMeeting(signature: any) {
+
+  ZoomMtg.init({
+     leaveUrl: this.leaveUrl,
+     isSupportAV: true,
+     success: (success: any) => {
+        console.log(success)
+
+        ZoomMtg.join({
+           signature: signature,
+           meetingNumber: this.meetingNumber,
+           userName: this.userName,
+           apiKey: this.apiKey,
+           userEmail: this.userEmail,
+           passWord: this.passWord,
+           success: (success: any) => {
+              console.log(success)
+           },
+           error: (error: any) => {
+              console.log(error)
+           }
+        })
+     },
+     error: (error: any) => {
+        console.log(error)
+     }
+  })
+
+}
+
 }
--- src/app/zoom/zoom.component.ts
+++ src/app/zoom/zoom.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ZoomService } from '../zoom.service';
+import { ZoomMtg } from '@zoomus/websdk';
+import { DOCUMENT } from '@angular/common';
+import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
+import { HttpClient } from '@angular/common/http';
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
 
 @Component({
   selector: 'app-zoom',
@@ -11,15 +18,31 @@ export class ZoomComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private zoomService: ZoomService
+    private zoomService: ZoomService,
+    @Inject(DOCUMENT) document: Document,
+    private http: HttpClient, 
   ) { }
 
   calendarCode = this.route.snapshot.queryParams.code;
   calendarError = this.route.snapshot.queryParams.error;
   calendarState = this.route.snapshot.queryParams.state;
 
+  signatureEndpoint = 'https://webdev-test-1.herokuapp.com'
+  apiKey = 'AI2DlbF1TGO2tOl1tku8_Q'
+  meetingNumber = 123456789
+  role = 0
+  leaveUrl = 'https://webdev-test-1.herokuapp.com'
+  userName = 'JWT Auth'
+  userEmail = ''
+  passWord = ''
+
+  
+
   ngOnInit(): void {
-    if(this.calendarCode && this.calendarState=="zoomcalendar") {
+    
+
+
+    if (this.calendarCode && this.calendarState == "zoomcalendar") {
       this.zoomService.getZoomCalendarToken(this.calendarCode).subscribe(
         data => {
           console.log(data);
@@ -28,9 +51,61 @@ export class ZoomComponent implements OnInit {
     }
   }
 
+  meeting() {
+    this.zoomService.getSignature();
+  }
+
+  getSignature() {
+
+    this.http.post(this.signatureEndpoint, {
+       meetingNumber: this.meetingNumber,
+       role: this.role
+    }).toPromise().then((data: any) => {
+       if(data.signature) {
+          console.log(data.signature)
+          this.startMeeting(data.signature)
+       } else {
+          console.log(data)
+       }
+    }).catch((error) => {
+       console.log(error)
+    })
+ 
+ }
+
+ startMeeting(signature: any) {
+
+  ZoomMtg.init({
+     leaveUrl: this.leaveUrl,
+     isSupportAV: true,
+     success: (success: any) => {
+        console.log(success)
+
+        ZoomMtg.join({
+           signature: signature,
+           meetingNumber: this.meetingNumber,
+           userName: this.userName,
+           apiKey: this.apiKey,
+           userEmail: this.userEmail,
+           passWord: this.passWord,
+           success: (success: any) => {
+              console.log(success)
+           },
+           error: (error: any) => {
+              console.log(error)
+           }
+        })
+     },
+     error: (error: any) => {
+        console.log(error)
+     }
+  })
+
+}
+
   addZoom() {
     window.open(`https://zoom.us/oauth/authorize?response_type=code&client_id=WBd6tpAsR2iZwgGdfzGsuQ&redirect_uri=https://webdev-test-1.herokuapp.com/appointment&state=zoomcalendar`, "_self")
-  
+
   }
 
 }
