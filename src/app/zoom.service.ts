a370672d12c4af1208e2b6e5d18be778219cd467
Sandeep kumar saw <sandeepkr1084@gmail.com>
2022-04-11 12:41:38 +0530

    zoom test

--- src/app/zoom.service.ts
+++ src/app/zoom.service.ts
@@ -10,6 +10,15 @@ export class ZoomService {
     private http: HttpClient, 
   ) { }
 
+  getZoomHeader() {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      
+      Authorization: `Bearer Bny47qq2TJWmaykCQ5JYkA:O8kbyPdHplJmqZbCSwnWL6rO5HATKU5A`
+    });
+  }
+
   getZoomCalendarToken(code: any){
     let cal_data = new URLSearchParams();
     cal_data.set('client_id', 'Bny47qq2TJWmaykCQ5JYkA');
@@ -19,6 +28,6 @@ export class ZoomService {
     //cal_data.set('grant_type', environment.OutlookCalendarCred.grant_type);
     cal_data.set('client_secret', 'O8kbyPdHplJmqZbCSwnWL6rO5HATKU5A');
     //let cal_data = `client_id=${environment.OutlookCalendarCred.client_id}&scope=${environment.OutlookCalendarCred.scope}&code=${code}&redirect_uri=${environment.OutlookCalendarCred.redirect_uri}&grant_type=${environment.OutlookCalendarCred.grant_type}&client_secret=${environment.OutlookCalendarCred.client_secret}`
-    return this.http.post<any>("https://zoom.us/oauth/token?grant_type="+cal_data.toString(), cal_data.toString(), {headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')})
+    return this.http.post<any>("https://zoom.us/oauth/token?grant_type=client_credentials", cal_data.toString(), {headers: this.getZoomHeader()})
   }
 }
