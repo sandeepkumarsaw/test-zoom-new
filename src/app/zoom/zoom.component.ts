46d3b7af1a95c8d00cbab62a91b916c8fdcdf62f
Sandeep kumar saw <sandeepkr1084@gmail.com>
2022-04-11 22:01:29 +0530

    zoom test token

--- src/app/zoom/zoom.component.ts
+++ src/app/zoom/zoom.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ZoomService } from '../zoom.service';
-import { ZoomMtg } from '@zoomus/websdk';
+// import { ZoomMtg } from '@zoomus/websdk';
 import { DOCUMENT } from '@angular/common';
 // import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
 import { HttpClient } from '@angular/common/http';
@@ -52,56 +52,56 @@ export class ZoomComponent implements OnInit {
   }
 
   meeting() {
-    this.getSignature();
+    //this.getSignature();
   }
 
-  getSignature() {
+//   getSignature() {
 
-    this.http.post(this.signatureEndpoint, {
-       meetingNumber: this.meetingNumber,
-       role: this.role
-    }).toPromise().then((data: any) => {
-       if(data.signature) {
-          console.log(data.signature)
-          this.startMeeting(data.signature)
-       } else {
-          console.log(data)
-       }
-    }).catch((error) => {
-       console.log(error)
-    })
+//     this.http.post(this.signatureEndpoint, {
+//        meetingNumber: this.meetingNumber,
+//        role: this.role
+//     }).toPromise().then((data: any) => {
+//        if(data.signature) {
+//           console.log(data.signature)
+//           this.startMeeting(data.signature)
+//        } else {
+//           console.log(data)
+//        }
+//     }).catch((error) => {
+//        console.log(error)
+//     })
  
- }
+//  }
 
- startMeeting(signature: any) {
+//  startMeeting(signature: any) {
 
-  ZoomMtg.init({
-     leaveUrl: this.leaveUrl,
-     isSupportAV: true,
-     success: (success: any) => {
-        console.log(success)
+//   ZoomMtg.init({
+//      leaveUrl: this.leaveUrl,
+//      isSupportAV: true,
+//      success: (success: any) => {
+//         console.log(success)
 
-        ZoomMtg.join({
-           signature: signature,
-           meetingNumber: this.meetingNumber,
-           userName: this.userName,
-           apiKey: this.apiKey,
-           userEmail: this.userEmail,
-           passWord: this.passWord,
-           success: (success: any) => {
-              console.log(success)
-           },
-           error: (error: any) => {
-              console.log(error)
-           }
-        })
-     },
-     error: (error: any) => {
-        console.log(error)
-     }
-  })
+//         ZoomMtg.join({
+//            signature: signature,
+//            meetingNumber: this.meetingNumber,
+//            userName: this.userName,
+//            apiKey: this.apiKey,
+//            userEmail: this.userEmail,
+//            passWord: this.passWord,
+//            success: (success: any) => {
+//               console.log(success)
+//            },
+//            error: (error: any) => {
+//               console.log(error)
+//            }
+//         })
+//      },
+//      error: (error: any) => {
+//         console.log(error)
+//      }
+//   })
 
-}
+// }
 
   addZoom() {
     window.open(`https://zoom.us/oauth/authorize?response_type=code&client_id=WBd6tpAsR2iZwgGdfzGsuQ&redirect_uri=https://webdev-test-1.herokuapp.com/appointment&state=zoomcalendar`, "_self")
